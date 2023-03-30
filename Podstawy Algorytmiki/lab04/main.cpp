10d3667a762f796daf08ffd8767cb9baecef33bb
Mateusz Kierepka <125149243+MateuszKierepka@users.noreply.github.com>
2023-03-30 08:06:33 +0200

    pa 30 03

--- Podstawy Algorytmiki/lab04/main.cpp
+++ Podstawy Algorytmiki/lab04/main.cpp
@@ -1,110 +1,121 @@
 #include <iostream>
-#include <string>
 #include <fstream>
 #include <sstream>
 
-using namespace std;
+using  namespace std;
 
-struct student{
+struct  student{
     string imie;
     string nazwisko;
-    int punkty;
+    int l_p;
 };
 
-void wczytajStudentow(student *&tab, int *liczba_studentow){
-    string sciezka, linia;
-    int liczbaStudentow;
-    fstream plik;
-    plik.open("C:\\Users\\Mateusz\\Desktop\\lab04\\studenci.csv");
-    if(!plik.is_open()){
-        cout<<"Nie mozna otworzyc pliku...";
-    }
-    char sredniki;
-    plik >> liczbaStudentow;
-    for(int i=0; i<2; i++){
-        plik >> sredniki;
-    }
-    for(int i=0; i<liczbaStudentow; i++){
-        plik >> linia;
-        stringstream ss(linia);
-        getline(ss, tab[i].imie, ';');
-        getline(ss, tab[i].nazwisko, ';');
-        ss >> tab[i].punkty;
-    }
-    *liczba_studentow = liczbaStudentow;
-    plik.close();
-}
-
-void wyswietlStudentow(student*tab, int n){
-    for(int i=0; i<n; i++){
-        cout<<"numer:"<<i+1<<"\nimie:"<<tab[i].imie<<endl;
-        cout<<"nazwisko:"<<tab[i].nazwisko<<endl;
-        cout<<"punkty:"<<tab[i].punkty<<endl;
-        cout<<endl;
-    }
+void wczytajStudentow(student*&tab, int n){
+    tab = new student[n];
 }
 
 void usunTabliceStudentow(student *&tab){
     delete [] tab;
 }
 
-void podzialNaDwieCzesci(student*tab, int n, int granica, int *koniec){
-    int i=0, j=n-1;
+void wyswietlStudentow(student* tab, int n){
+    for(int i=0; i<n; i++){
+        cout<<tab[i].imie<<" "<<tab[i].nazwisko<<" "<<tab[i].l_p<<endl;
+    }
+    cout<<endl;
+}
+
+void wyswietlStudentowP(student* tab, int n, int p){
+    cout<<"Studenci, ktorzy otrzymali <=10 punktów: "<<endl;
+    for(int i=0; i<p; i++){
+        cout<<tab[i].imie<<" "<<tab[i].nazwisko<<" "<<tab[i].l_p<<endl;
+    }
+    cout<<"Studenci, ktorzy otrzymali >10 punktów:"<<endl;
+    for(int i=p; i<n; i++){
+        cout<<tab[i].imie<<" "<<tab[i].nazwisko<<" "<<tab[i].l_p<<endl;
+    }
+    cout<<endl;
+}
+
+void wyswietlStudentowP2(student* tab, int n, int p, int g){
+    cout<<"Studenci, ktorzy otrzymali liczbe punktow podzielnych przez 3 :"<<endl;
+    for(int i=0; i<=p; i++){
+        cout<<tab[i].imie<<" "<<tab[i].nazwisko<<" "<<tab[i].l_p<<endl;
+    }
+    cout<<"Studenci, ktorzy otrzymali liczbe punktow podzielnych przez 3 z reszta 1 :"<<endl;
+    for(int i=p+1; i<g; i++){
+        cout<<tab[i].imie<<" "<<tab[i].nazwisko<<" "<<tab[i].l_p<<endl;
+    }
+    cout<<"Studenci, ktorzy otrzymali liczbe punktow podzielnych przez 3 z reszta 2 :"<<endl;
+    for(int i=g; i<n; i++){
+        cout<<tab[i].imie<<" "<<tab[i].nazwisko<<" "<<tab[i].l_p<<endl;
+    }
+    cout<<endl;
+}
+
+int wczytywanieDanych(student*& tab){
+    string sciezka, linia, pomoc;
+    int liczbaStudentow;
+    ifstream plik;
+    char sredniki;
+    sciezka="C:\\Users\\Mateusz\\Desktop\\Studia\\SEMESTR-II\\university-part-I\\Podstawy Algorytmiki\\lab04\\studenci.csv";
+    plik.open(sciezka);
+    if(!plik.is_open()){
+        cout<<"Nie udalo sie otworzyc pliku...\n";
+    }
+    plik >> liczbaStudentow;
+    wczytajStudentow(tab,liczbaStudentow);
+    for(int i=0; i<2; i++){
+        plik>>sredniki;
+    }
+    for(int i=0; i<liczbaStudentow; i++){
+        plik>>linia;
+        stringstream ss(linia);
+        getline(ss,tab[i].imie,';');
+        getline(ss, tab[i].nazwisko, ';');
+        getline(ss, pomoc);
+        tab[i].l_p=atoi(pomoc.c_str());
+    }
+    plik.close();
+    return liczbaStudentow;
+}
+
+int podzial2czesci(student*& tab, int l_s, int granica){
+    int i=0;
+    int j=l_s-1;
     while(i < j){
-        while(tab[i].punkty <= granica && i<j){
+        while (tab[i].l_p <= granica && i < j){
             i++;
         }
-        while(tab[j].punkty>granica && i<j){
+        while (tab[j].l_p > granica && i < j){
             j--;
         }
         if(i < j){
             swap(tab[i], tab[j]);
-            i+=1;
-            j-=1;
+            i++;
+            j--;
         }
     }
-    if(tab[i].punkty <= granica){
-        *koniec=i+1;
+    if(tab[i].l_p <= granica){
+        return i+1;
     }
     else{
-        *koniec=i;
+        return i;
     }
 }
 
-void podzialNaDwieCzesci_wyswietl(student*tab, int n, int granica, int koniec){
-    for(int i=0; i<n; i++){
-        if(i==0){
-            cout<<"Studenci ktorzy otrzymali <="<<granica<<" punktow:"<<endl;
-            cout<<endl;
-        }
-        if(i < koniec){
-            cout<<"Imie: "<<tab[i].imie<<endl;
-            cout<<"Nazwisko: "<<tab[i].nazwisko<<endl;
-            cout<<"Punkty: "<<tab[i].punkty<<endl;
-            continue;
-        }
-        if(i==koniec){
-            cout<<endl;
-            cout<<"Studenci ktorzy otrzymali >"<<granica<<" punktow:"<<endl;
-        }
-        if(i >= koniec){
-            cout<<"Imie: "<<tab[i].imie<<endl;
-            cout<<"Nazwisko: "<<tab[i].nazwisko<<endl;
-            cout<<"Punkty: "<<tab[i].punkty<<endl;
-        }
-    }
-}
-
-void podzialNaTrzyCzesci(student *tab, int n, int *koniec1, int *koniec2){
-    int i=-1, j=0, k=n;
-    while(j < k){
-        if(tab[j].punkty%3==0){
+void podzial3czesci(student*& tab, int l_s, int* tab2){
+    int i=-1;
+    int j=0;
+    int k=l_s;
+    while (j<k){
+        if(tab[j].l_p%3==0){
             i++;
             swap(tab[i], tab[j]);
             j++;
         }
         else{
-            if(tab[j].punkty%3==2){
+            if(tab[j].l_p%3==2){
                 k--;
                 swap(tab[k], tab[j]);
             }
@@ -113,73 +124,31 @@ void podzialNaTrzyCzesci(student *tab, int n, int *koniec1, int *koniec2){
             }
         }
     }
-    *koniec1=i;
-    *koniec2=k;
-}
-
-void podzialNaTrzyCzesci_wyswietl(student *tab, int n, int kon1, int kon2){
-    for(int i=0; i<n; i++){
-        if(i <= kon1){
-            if(i==0){
-                cout<<"Studenci ktorzy maja liczbe punktow podzielna przez 3"<<endl;
-            }
-            cout<<"Imie: "<<tab[i].imie<<endl;
-            cout<<"Nazwisko: "<<tab[i].nazwisko<<endl;
-            cout<<"Punkty: "<<tab[i].punkty<<endl;
-        }
-        if(i>kon1 && i<kon2){
-            if(i==kon1+1){
-                cout<<"Studenci ktorzy maja liczbe punktow podzielna przez 3 z reszta 1"<<endl;
-            }
-            cout<<"Imie: "<<tab[i].imie<<endl;
-            cout<<"Nazwisko: "<<tab[i].nazwisko<<endl;
-            cout<<"Punkty: "<<tab[i].punkty<<endl;
-        }
-        if(i>=kon2){
-            if(i==kon2){
-                cout<<"Studenci ktorzy maja liczbe punktow podzielna przez 3 z reszta 2"<<endl;
-            }
-            cout<<"Imie: "<<tab[i].imie<<endl;
-            cout<<"Nazwisko: "<<tab[i].nazwisko<<endl;
-            cout<<"Punkty: "<<tab[i].punkty<<endl;
-        }
-    }
+    tab2[0]=i;
+    tab2[1]=k;
 }
 
 int main() {
-int wybor, granica, *koniec, *koniec1, *koniec2, kon, kon1, kon2, l_s=0, *wsk;
-koniec1=&kon1;
-koniec2=&kon2;
-koniec=&kon;
-wsk=&l_s;
-cout<<"Wybierz program :\n1 - Wczytanie danych\n2 - Podzial zbioru na dwie czesci\n3 - Podzial zbioru na trzy czesci\n";
-cin>>wybor;
-    switch (wybor) {
-        case 1: {
-            cout << "Program wczytujacy dane...\n";
-            student *tab2=new student[14];
-            wczytajStudentow(tab2, wsk);
-            wyswietlStudentow(tab2, l_s);
-            break;
+    while(true) {
+        student *tab;
+        int wybor;
+        int l_s = wczytywanieDanych(tab);
+        cout << "Wybierz program :\n1 - Wczytywanie danych\n2 - podzial na 2 czesci\n3 - podzial na 3 czesci\n";
+        cin>>wybor;
+        if (wybor == 1) {
+            wyswietlStudentow(tab, l_s);
         }
-        case 2: {
-            cout << "Program do podzialu zbioru na dwie czesci...\n";
-            student *tab2=new student[14];
-            cout<<"Podaj granice :\n"; cin>>granica;
-            podzialNaDwieCzesci(tab2, l_s, granica, koniec);
-            podzialNaDwieCzesci_wyswietl(tab2, l_s, granica, kon);
-            break;
+        else if (wybor == 2) {
+            int gr = podzial2czesci(tab, l_s, 10);
+            wyswietlStudentowP(tab, l_s, gr);
         }
-        case 3: {
-            cout << "Program do podzialu zbioru na 3 czesci...\n";
-            student *tab2=new student[14];
-            podzialNaTrzyCzesci(tab2, l_s, koniec1, koniec2);
-            podzialNaTrzyCzesci_wyswietl(tab2, l_s, kon1, kon2);
-            break;
+        else if (wybor == 3) {
+            int tab2[2];
+            podzial3czesci(tab, l_s, tab2);
+            wyswietlStudentowP2(tab, l_s, tab2[0], tab2[1]);
         }
-        default: {
-            cout << "Blad danych.\n";
+        else {
+            return 0;
         }
     }
-            return 0;
 }
